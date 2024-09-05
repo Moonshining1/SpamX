387c89c18e67f8d6d07785ba790221649adf829c
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-09-05 19:27:55 +0530

    Update config.py

--- SpamX/config.py
+++ SpamX/config.py
@@ -6,32 +6,32 @@ from dotenv import load_dotenv
 if os.path.exists(".env"):
     load_dotenv(".env")
 
-API_ID = int(env("API_ID", "25614292"))
+API_ID = int(env("API_ID", None))
 if API_ID is None:
     LOGS.error("Please set your API_ID!")
     quit(1)
 
-API_HASH = str(env("API_HASH", "59ee8005ce6b056fa639d956f028eeeb"))
+API_HASH = str(env("API_HASH", None))
 if API_HASH is None:
     LOGS.error("Please set your API_HASH!")
     quit(1)
 
-ASSISTANT_TOKEN = str(env("ASSISTANT_TOKEN", "6507210376:AAEIpz1aOzQ8WLg7MDUjg858eygO-CiDyLo"))
+ASSISTANT_TOKEN = str(env("ASSISTANT_TOKEN", None))
 if ASSISTANT_TOKEN is None:
     LOGS.error("Please set your ASSISTANT_TOKEN!")
     quit(1)
 
-OWNER_ID = int(env("OWNER_ID", "6723933089"))
+OWNER_ID = int(env("OWNER_ID", None))
 if OWNER_ID is None:
     LOGS.error("Please set your OWNER_ID!")
     quit(1)
 
-DATABASE_URL = str(env("DATABASE_URL", "postgresql://xrlkskby:gobwyeqocauwmdrggqom@alpha.mkdb.sh:5432/rjfvbvce"))
+DATABASE_URL = str(env("DATABASE_URL", None))
 if DATABASE_URL is None:
     LOGS.error("Please set your DATABASE_URL!")
     quit(1)
 
-LOGGER_ID = str(env("LOGGER_ID", "-1002177358647"))
+LOGGER_ID = str(env("LOGGER_ID", None))
 if LOGGER_ID is None:
     LOGS.error("Please set your LOGGER_ID! (make a private group add assistant in that group & promote as Admin!)")
     quit(1)
